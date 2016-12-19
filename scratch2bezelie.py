e411b53fe75f1440df2a943b50df85d5f5b26de1
Daisuke IMAI <hine.gdw@gmail.com>
2016-12-19 23:43:10 +0900

    接続するScratchのホストを指定できるように

--- scratch2bezelie.py
+++ scratch2bezelie.py
@@ -6,6 +6,7 @@ This software is released under the MIT License.
 http://opensource.org/licenses/mit-license.php
 '''
 
+import sys
 import time
 import threading
 import scratch
@@ -26,6 +27,14 @@ class Receiver(object):
 
 if __name__ == '__main__':
 
+    scratchhost = "127.0.0.1"
+
+    # 引数処理
+    param = sys.argv
+    if len(param) > 1:
+        # 引数がついていればScratchの接続先として利用
+        scratchhost = param[1]
+
     # Bezelieと接続しコントロールするためのインスタンス生成
     bez = bezelie.Control()
     bez.setTrim(head=7, back=2, stage=2)
@@ -33,10 +42,12 @@ if __name__ == '__main__':
     # Scratch接続のためのインスタンス生成
     rcv = Receiver(bez)
     rsc = scratch.RemoteSensorConnection(rcv.broadcast_handler, rcv.sonsor_update_handler)
+    rsc.connect(scratchhost)
 
     while(True):
         try:
             time.sleep(1)
         except KeyboardInterrupt:
             print " Interrupted by Keyboard"
+            rsc.disconnect()
             exit()
